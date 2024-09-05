89a10a6276628f5d9adc88092d5d3c0b1d708c42
Fred Willmore <fredwillmore@gmail.com>
2024-09-05 14:13:21 -0700

    added Libertarian party

--- db/seeds/parties.rb
+++ db/seeds/parties.rb
@@ -6,6 +6,7 @@ Party.destroy_all if $options['destroy_all'] == 'true'
   "ID" => "Independent Democrat",
   'D' => "Democrat",
   'R' => "Republican",
+  'L' => "Libertarian",
 }.each do |abbreviation, name|
 
   Party.find_or_create_by(
