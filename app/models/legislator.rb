8bc0f51a3b2c9bc355a674ff28bc1b5ed4170296
Fred Willmore <fredwillmore@gmail.com>
2024-09-10 09:11:51 -0700

    added :terms association

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -3,6 +3,8 @@ class Legislator < ApplicationRecord
   validates :first_name, :last_name, :bioguide_id, presence: true
   validates :birth_year, numericality: { other_than: 0 }
 
+  has_many :terms
+
   def to_s
     "first_name: #{first_name}, last_name: #{last_name}, bioguide_id: #{bioguide_id}"
   end
