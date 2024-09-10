1cec0a6d761fde4a9ca4296669aff7223a0bdec3
Fred Willmore <fredwillmore@gmail.com>
2024-09-10 09:25:59 -0700

    run individual seed tasks in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+require 'csv'
+
+Rake::Task['db:seed:parties'].invoke
+Rake::Task['db:seed:states'].invoke
+Rake::Task['db:seed:filing_types'].invoke
+Rake::Task['db:seed:legislators'].invoke
+Rake::Task['db:seed:party_affiliations'].invoke
+Rake::Task['db:seed:terms'].invoke
+Rake::Task['db:seed:disclosures'].invoke
