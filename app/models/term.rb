76a04827f7ca21abf768923d8c2009001c75e33a
Fred Willmore <fredwillmore@gmail.com>
2024-09-10 09:13:16 -0700

    added by_state_district_year for looking up term and legislator

--- app/models/term.rb
+++ app/models/term.rb
@@ -6,6 +6,15 @@ class Term < ApplicationRecord
   validates :start_year, numericality: { other_than: 0 }
 
   def to_s
-    "#{legislator.name} - #{state.name} - #{member_type} - #{start_year}-#{end_year}"
+    "#{legislator.name} - #{state.name} - #{district} - #{member_type} - #{start_year}-#{end_year}"
+  end
+
+  def self.by_state_district_year(state, district, year)
+    where(
+      state: state,
+      district: district
+    ).where(
+      "start_year <= ? AND (end_year >= ? OR end_year IS NULL)", year, year
+    )
   end
 end
