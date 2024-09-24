2cc092569ac9a63b00802b6367b37961afb94016
Fred Willmore <fredwillmore@gmail.com>
2024-09-24 11:50:47 -0700

    FactoryBot factories created for application models

---
+++ spec/factories.rb
@@ -0,0 +1,32 @@
+FactoryBot.define do
+  factory :income_type do
+    
+  end
+
+  factory :asset do
+  end
+
+  factory :disclosure do
+    legislator
+    filing_type
+    state
+  end
+
+  factory :filing_type do
+  end
+
+  factory :legislator do
+    first_name { "Test" }
+    last_name { "Test" }
+    bioguide_id { "ABC123" }
+    birth_year { 1977 }
+  end
+
+  factory :state do
+    abbreviation {"IL"}
+    name {"Illinois"}
+  end
+
+  factory :transaction do
+  end
+end
