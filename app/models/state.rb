ccb4eeabb467fd1e62e4c223b889594172f6fd6a
Fred Willmore <fredwillmore@gmail.com>
2024-10-01 15:56:53 -0700

    State has_many :terms

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,16 @@
 class State < ApplicationRecord
+  has_many :terms
+
+  def congress_info(congress_number: )
+    OpenStruct.new(
+      {
+        state: self,
+        name: name,
+        abbreviation: abbreviation,
+        house: terms.house(congress: congress_number).joins(:legislator).order('legislators.last_name').map(&:legislator),
+        senate: terms.senate(congress: congress_number).joins(:legislator).order('legislators.last_name').map(&:legislator)
+      }
+    )
+  end
+
 end
