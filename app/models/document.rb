1362d252f2ec2adbf4e1d24f411302f1ef010a1d
Fred Willmore <fredwillmore@gmail.com>
2024-10-01 15:55:43 -0700

    Document support for ignoring select records with kn0own issues

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,27 @@
 class Document < ApplicationRecord
   belongs_to :disclosure
 
+  scope :good, -> { where.not(external_id: ids_with_issues) }
+  scope :not_good, -> { where(external_id: ids_with_issues) }
+
+  # TODO: address these issues eventually. I think the majority might just be an issue with transactions_header_regex
+  def ids_with_issues
+    [
+      "10010275",
+      "10010412",
+      "10010412",
+      "10058974",
+      "10016774",
+      "10040761",
+      "10015486",
+      "10021225",
+      "10026542",
+      "10035501",
+      "10046177",
+      "10048922"
+    ]
+  end
+
   def document_url
     "https://disclosures-clerk.house.gov/public_disc/#{disclosure.filing_type.document_directory}/#{disclosure.year}/#{external_id}.pdf"
   end
