ee3ab8c77d2bc0e1fb7b7d10b9df2312a8278242
Fred Willmore <fredwillmore@gmail.com>
2024-10-01 17:22:39 -0700

    RequestAccumulator has an array of requests

--- app/services/request_accumulator.rb
+++ app/services/request_accumulator.rb
@@ -1,13 +1,14 @@
 class RequestAccumulator
+  attr_accessor :requests
   attr_accessor :request_batch_file
 
   def initialize(request_batch_file = nil)
+    @requests = []
     @request_batch_file = request_batch_file
   end
 
-  def puts(payload = nil)
-    debugger
-    # request_batch_file.puts payload
+  def <<(request)
+    requests << request
   end
 
 end
