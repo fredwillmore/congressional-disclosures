aebd3ada459224bca3ff6de1ee4b2390cedd5c1f
Fred Willmore <fredwillmore@gmail.com>
2024-11-05 11:35:42 -0800

    LegislatorController actions "show", "disclosures", and "voting_record" implemented

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,4 +1,21 @@
-class LegislatorController < ApplicationController
+class LegislatorsController < ApplicationController
   def show
+    legislator = Legislator.find(params[:id])
+    term = legislator.terms.find_by(congress_id: params[:congress_id])
+    
+    render locals: { legislator: legislator, term: term }
+  end
+  
+  def disclosures
+    legislator = Legislator.find(params[:id])
+    term = Term.find(params[:term])
+    disclosures = legislator.disclosures.where(year: [(term.start_year)..(term.end_year)])
+    
+    render locals: { legislator: legislator, term: term, disclosures: disclosures }
+  end
+
+  def voting_record
+    legislator = Legislator.find(params[:id])
+    term = Term.find(params[:term])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'legislator/show'
+  resources :legislators, only: [:show] do
+    member do
+      get 'disclosures'
+      get 'voting_record'
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
